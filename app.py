26fa06ab921a2eee50a3bcbe742c20979fd67937
kcurtis0618 <137686365+kcurtis0618@users.noreply.github.com>
2023-11-16 19:55:06 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+# 設定 Channel Access Token 與 Channel Secret
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # 取得 LINE 請求頭的資料
+    signature = request.headers['X-Line-Signature']
+
+    # 取得請求的內容
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        # 驗證簽章
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # 收到文字訊息時的處理邏輯
+    user_message = event.message.text
+    reply_message = f'你說了：{user_message}'
+    
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_message)
+    )
+
+if __name__ == "__main__":
+    app.run(port=5000)
